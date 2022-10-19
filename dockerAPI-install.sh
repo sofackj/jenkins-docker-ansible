3c9e6fe3eb43089af7e93f6659ef70f241051ac7
Julien SOFACK-LEMAIRE <53252137+sofackj@users.noreply.github.com>
2022-10-19 16:17:44 +0200

    Change Dockerfile content

--- dockerAPI-install.sh
+++ dockerAPI-install.sh
@@ -26,29 +26,23 @@ EOF
 cat <<EOF > Dockerfile
 FROM ubuntu:18.04
 
-LABEL maintainer="Bibin Wilson <bibinwilsonn@gmail.com>"
-
 # Make sure the package repository is up to date.
 RUN apt-get update && \
-    apt-get -qy full-upgrade && \
     apt-get install -qy git && \
 # Install a basic SSH server
     apt-get install -qy openssh-server && \
     sed -i 's|session    required     pam_loginuid.so|session    optional     pam_loginuid.so|g' /etc/pam.d/sshd && \
     mkdir -p /var/run/sshd && \
-# Install JDK 8 (latest stable edition at 2019-04-01)
-    apt-get install -qy openjdk-8-jdk && \
-# Install maven
-    apt-get install -qy maven && \
+# Install JDK 11
+    apt-get install -qy openjdk-11-jdk && \
 # Cleanup old packages
     apt-get -qy autoremove && \
 # Add user jenkins to the image
     adduser --quiet jenkins && \
 # Set password for the jenkins user (you may want to alter this).
-    echo "jenkins:jenkins" | chpasswd && \
+    echo "jenkins:password" | chpasswd && \
     mkdir /home/jenkins/.m2
 
-#ADD settings.xml /home/jenkins/.m2/
 # Copy authorized keys
 COPY .ssh/authorized_keys /home/jenkins/.ssh/authorized_keys
 
