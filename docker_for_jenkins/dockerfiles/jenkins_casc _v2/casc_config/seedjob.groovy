085b43b77c34c6383c355bea82a9de70da512922
Julien <sofa.kreutz@yahoo.fr>
2022-11-18 18:19:50 +0100

    Update structure

---
+++ docker_for_jenkins/dockerfiles/jenkins_casc _v2/casc_config/seedjob.groovy
@@ -0,0 +1,31 @@
+// create an array with our two pipelines
+pipelines = ["first-test", "second-test"]
+
+// iterate through the array and call the create_pipeline method
+pipelines.each {
+    pipeline ->
+    println "Creating pipeline ${pipeline}"
+    create_pipeline(pipeline)
+}
+
+def create_pipeline(String name) {
+    pipelineJob(name) {
+        definition {
+            cps {
+                sandbox(true)
+                script(
+                    """
+node(){
+    stage("first"){
+        echo "Hello World !!!"
+    }
+    stage("second"){
+        echo "Bye World !!!"
+    }
+}
+                    """
+                )
+            }
+        }
+    }
+}
