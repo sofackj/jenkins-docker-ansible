f93659cd5bd219a9be19d7c09971f30d153083e5
Julien <sofa.kreutz@yahoo.fr>
2022-11-21 16:01:06 +0100

    update after test

--- docker_for_jenkins/dockerfiles/jenkins_casc_v2/casc_config/seedjob.groovy
+++ docker_for_jenkins/dockerfiles/jenkins_casc_v2/casc_config/seedjob.groovy
@@ -15,46 +15,12 @@ def create_pipeline(String name) {
                 sandbox(true)
                 script(
                     """
-node("dockerHost"){
-    stage('Import repositories'){
-        dir("build_project"){
-            checkout([$class: 'GitSCM',
-                branches: [[name: '*/dev']],
-                doGenerateSubmoduleConfigurations: false,
-                extensions: [[$class: 'CleanCheckout']],
-                submoduleCfg: [],
-                userRemoteConfigs: [[credentialsId: 'my-git-credentials', url: 'https://github.com/sofackj/jenkins-docker-ansible.git']]
-            ])
-        }
-        dir("ansible_project"){
-            checkout([$class: 'GitSCM',
-                branches: [[name: '*/dev']],
-                doGenerateSubmoduleConfigurations: false,
-                extensions: [[$class: 'CleanCheckout']],
-                submoduleCfg: [],
-                userRemoteConfigs: [[credentialsId: 'my-git-credentials', url: 'https://github.com/sofackj/docker-for-projects.git']]
-            ])
-        }
+node(){
+    stage("first"){
+        echo "Hello World !!!"
     }
-    stage ('Environment variables') {
-        env.ANSIBLE_DIR = "ansible_project/ansible/ansible_volumes/host_interaction"
-        env.ANSIBLE_PLAYBOOK = "host-int-playbook.yml"
-    }
-    stage ('Build and Deployment') {
-        def ANSIBLE_IMG = docker.build (
-            "my-ansible",
-            "./build_project/docker_for_jenkins/dockerfiles/ansible_controller/"
-            )
-        ANSIBLE_IMG.inside ('-u root') {
-            dir ("${ANSIBLE_DIR}") {
-                ansiblePlaybook (
-                    playbook: "${ANSIBLE_PLAYBOOK}"
-                )  
-            }
-        }
-    }
-    stage("Cleanup"){
-        cleanWs()
+    stage("second"){
+        echo "Bye World !!!"
     }
 }
                     """
