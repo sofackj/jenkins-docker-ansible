e20eb6adc6970e03ed27047c6f5d2f743f9ceaac
Julien <sofa.kreutz@yahoo.fr>
2022-11-21 16:50:13 +0100

    update after test

--- docker_for_jenkins/dockerfiles/jenkins_casc_v2/casc_config/seedjob.groovy
+++ docker_for_jenkins/dockerfiles/jenkins_casc_v2/casc_config/seedjob.groovy
@@ -1,36 +1,36 @@
-// create an array with our two pipelines
-pipelines = ["first-test"]
+// // create an array with our two pipelines
+// pipelines = ["first-test"]
 
-// iterate through the array and call the create_pipeline method
-pipelines.each {
-    pipeline ->
-    println "Creating pipeline ${pipeline}"
-    create_pipeline(pipeline)
-}
+// // iterate through the array and call the create_pipeline method
+// pipelines.each {
+//     pipeline ->
+//     println "Creating pipeline ${pipeline}"
+//     create_pipeline(pipeline)
+// }
 
-def create_pipeline(String name) {
-    pipelineJob(name) {
-        parameters {
-            booleanParam('FLAG', true)
-            choiceParam('OPTION', ['option 1 (default)', 'option 2', 'option 3'])
-        }
-        definition {
-            cpsScm {
-                scm {
-                    git {
-                        branch('*/dev')
-                        remote {
-                            url('https://github.com/sofackj/jenkins-docker-ansible.git')
-                            credentials('my-git-credentials')
-                        }
-                        extensions {
-                            cleanAfterCheckout()
-                        }
+// def create_pipeline(String name) {
+pipelineJob(name) {
+    parameters {
+        booleanParam('FLAG', true)
+        choiceParam('OPTION', ['option 1 (default)', 'option 2', 'option 3'])
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    branch('*/dev')
+                    remote {
+                        url('https://github.com/sofackj/jenkins-docker-ansible.git')
+                        credentials('my-git-credentials')
                     }
-                    
+                    extensions {
+                        cleanAfterCheckout()
+                    }
                 }
-                scriptPath("jenkinsfile")
+                
             }
+            scriptPath("jenkinsfile")
         }
     }
 }
+// }
