f3640b640bbfb17e7b952b050bafc952987f9406
Julien <sofa.kreutz@yahoo.fr>
2022-11-25 17:01:25 +0100

    update structure

--- jenkins_casc/casc_config/jobs/main.groovy
+++ jenkins_casc/casc_config/jobs/main.groovy
@@ -23,40 +23,40 @@ node('jenkins') {
     }
 }
 // Check containers
-pipelineJob("check-container") {
-        definition {
-            cps {
-                sandbox(true)
-                script("""
-def list_containers = ["registry"]
-def STATUS
-node('dockerHost'){
-    stage('Define variables') {
-    }
-    list_containers.each { container ->
-        stage("Check Status ${container}") {
-            echo "HELLO"
-        }
-        stage("Specs ${container}") {
-            def NETWORK = shell_output ("docker inspect -f '{{ .HostConfig.NetworkMode }}' ${container}")
-            echo "STATUS      => ${shell_output ("docker exec registry echo OK")}"
-            echo "ID          => ${shell_output ("docker inspect -f '{{ .Id }}' ${container}")}"
-            echo "Image       => ${shell_output ("docker inspect -f '{{ .Config.Image }}' ${container}")}"
-            echo "Network     => ${shell_output ("docker inspect -f '{{ .HostConfig.NetworkMode }}' ${container}")}"
-            echo "IP Address  => ${shell_output ("docker inspect -f '{{ .NetworkSettings.Networks.${NETWORK}.IPAddress }}' ${container}")}"
-        }
-    }
-}
-def shell_output (String command) {
-    def command_output = sh(
-        script: command,
-        returnStdout: true
-        ).trim()
-    return command_output
-}
-                """)
-        }
-    }
-}
+// pipelineJob("check-container") {
+//         definition {
+//             cps {
+//                 sandbox(true)
+//                 script("""
+// def list_containers = ["registry"]
+// def STATUS
+// node('dockerHost'){
+//     stage('Define variables') {
+//     }
+//     list_containers.each { container ->
+//         stage("Check Status ${container}") {
+//             echo "HELLO"
+//         }
+//         stage("Specs ${container}") {
+//             def NETWORK = shell_output ("docker inspect -f '{{ .HostConfig.NetworkMode }}' ${container}")
+//             echo "STATUS      => ${shell_output ("docker exec registry echo OK")}"
+//             echo "ID          => ${shell_output ("docker inspect -f '{{ .Id }}' ${container}")}"
+//             echo "Image       => ${shell_output ("docker inspect -f '{{ .Config.Image }}' ${container}")}"
+//             echo "Network     => ${shell_output ("docker inspect -f '{{ .HostConfig.NetworkMode }}' ${container}")}"
+//             echo "IP Address  => ${shell_output ("docker inspect -f '{{ .NetworkSettings.Networks.${NETWORK}.IPAddress }}' ${container}")}"
+//         }
+//     }
+// }
+// def shell_output (String command) {
+//     def command_output = sh(
+//         script: command,
+//         returnStdout: true
+//         ).trim()
+//     return command_output
+// }
+//                 """)
+//         }
+//     }
+// }
 
 
