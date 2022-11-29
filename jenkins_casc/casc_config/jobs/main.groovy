d3be9e925e2600f904d41b102680a35901f8bf4b
Julien <sofa.kreutz@yahoo.fr>
2022-11-29 14:31:29 +0100

    update structure

--- jenkins_casc/casc_config/jobs/main.groovy
+++ jenkins_casc/casc_config/jobs/main.groovy
@@ -27,48 +27,9 @@ pipelineJob("check") {
     configure { project ->
         project / 'definition' {
             'script'('''
-            node(&apos;dockerHost&apos;){
-    //
-    stage(&apos;Define variables&apos;){
-        env.registry_status
-        env.registry_output
-        env.image_list
-    }
-    //
-    stage(&apos;Check Registry Status&apos;){
-        // Check if the registry container is running
-        try {
-           registry_status = sh(
-            script: &quot;docker exec registry sh -c &apos;echo Check OK&apos;&quot;,
-            returnStdout: true
-            ).trim()
-            echo &quot;${registry_status}&quot;
-        // If exit error occurs, the pipeline fails
-        } catch (err) {
-            error(&quot;Check if the &apos;registry&apos; container is running properly&quot;)
-        }
-    }
-    //
-    stage(&apos;Display registered images&apos;){
-        // Catch the output listing images list
-        registry_output = sh(
-            script: &quot;curl -s http://localhost:5000/v2/_catalog&quot;,
-            returnStdout: true
-            ).trim()
-        // Convert the precedent string output to a list
-        images_list = convert_registry_output(registry_output)
-        echo &quot;${images_list}&quot;
-    }
-}
-// Useful function for the pipeline
-def convert_registry_output(String name) {
-    my_list = []
-    pre_list = name.split(&apos;:&apos;)[1][1..-3].split(&apos;,&apos;)
-    pre_list.each { item -&gt;
-            my_list &lt;&lt; item[1..-2]
-        }
-    return my_list
-}
+            node('nodeHost'){
+                echo "Hello World"
+            }
             ''')
         }
     }
